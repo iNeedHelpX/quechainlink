981035e9d7feb42252d8fdd147700e56022e5560
Julia Pak <jpak0444@gmail.com>
2022-05-17 09:23:02 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/juliapak/quechainlink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.juliapak.quechainlink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors/colours_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//main colours
+const bg = Color.fromARGB(255, 44, 41, 72);
+const containerback = Color.fromARGB(255, 142, 73, 255);
+const brightpink = Color.fromARGB(255, 255, 8, 186);
+const brightpink1 = Color.fromARGB(255, 253, 0, 97);
+const textpurple = Color.fromARGB(255, 72, 47, 246);
+const orangetext = Color.fromARGB(255, 246, 161, 47);
+const gold = Color.fromARGB(255, 253, 229, 124);
+const greenpastel = Color.fromARGB(255, 113, 185, 146);
+const tabicon = Color.fromARGB(255, 252, 0, 165);
+const tabBar = Color.fromARGB(255, 93, 55, 255);
+
+//helpful for text and errors!!
+const filltext = Color.fromARGB(255, 82, 83, 137);
+const textgreyblue = Color.fromARGB(255, 53, 53, 61);
+const textturq = Color.fromARGB(255, 60, 190, 200);
+const errorcolor = Color.fromARGB(255, 253, 0, 84);
+const lightpink = Color.fromARGB(255, 221, 208, 243);
+const logout = Color.fromARGB(255, 11, 16, 137);
