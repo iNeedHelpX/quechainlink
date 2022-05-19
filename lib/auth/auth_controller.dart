bfb9b70a7ad59f09381780335307d2757afef00a
Julia Pak <jpak0444@gmail.com>
2022-05-18 20:01:26 -0400

    auth

--- lib/auth/auth_controller.dart
+++ lib/auth/auth_controller.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   //google login
+  FirebaseAuth _auth = FirebaseAuth.instance;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quechainlink/start/start.dart';
 
 Future<void> main() async {
@@ -7,5 +8,7 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
 //change this here
-  runApp(Start());
+  runApp(
+    Start(),
+  );
 }
