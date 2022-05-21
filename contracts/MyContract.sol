e580753f18b502387231e4210d752ecd87448dcc
Julia Pak <jpak0444@gmail.com>
2022-05-21 12:59:35 -0400

    remote

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.1 <0.9.0;
+pragma solidity ^0.8.13;
 //this is my contract
+import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
