7f6a5924212124a80baad8821ce456fc6ba10ca5
Julia Pak <jpak0444@gmail.com>
2022-05-23 05:47:17 -0400

    good

--- lib/pages/peoplepage.dart
+++ lib/pages/peoplepage.dart
@@ -28,7 +28,15 @@ class _PeoplePageState extends State<PeoplePage> {
                 imgurl:
                     "https://firebasestorage.googleapis.com/v0/b/quechainlink.appspot.com/o/woman1.jpeg?alt=media&token=187682b8-5207-40f7-992d-fb2bca892a67",
                 deliverfee: "\$30",
-                name: "Janelle waits in line"),
+                name: "Janelle"),
+            SizedBox(
+              height: 10,
+            ),
+            ItemContainer(
+                imgurl:
+                    "https://firebasestorage.googleapis.com/v0/b/quechainlink.appspot.com/o/moonrisewoman.jpeg?alt=media&token=575ca11c-394b-4a69-939c-c46b8f8db42f",
+                deliverfee: "\$30",
+                name: "Julia")
           ],
         ),
       ),
