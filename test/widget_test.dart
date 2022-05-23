463d5c02b6cdca497f83bad4d71575b46dfcd2e9
Julia Pak <jpak0444@gmail.com>
2022-05-22 20:12:12 -0400

    tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:quechainlink/start/start.dart';
 
+//for flutter ui front end
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
