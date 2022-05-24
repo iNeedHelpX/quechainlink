2379c88a52e062d2b58a4264a2031b96e42c87dc
Julia Pak <jpak0444@gmail.com>
2022-05-23 21:19:07 -0400

    ok

--- contracts/Assign.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.1 <0.9.0;
-
-import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-//meant to book the person on list
--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.8.13;
 
 import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-//my contract
+//my contract that makes upgradable
