8cfe42e0d7e2c85b59e0c01fbffad33169392e56
Julia Pak <jpak0444@gmail.com>
2022-05-23 21:19:02 -0400

    ok

--- contracts/Upgradeable.sol
+++ contracts/Upgradeable.sol
@@ -1,3 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-// this is the upgradeable contract part
+
+import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+//my contract that makes upgradable
