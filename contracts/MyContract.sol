d92143781add3c67660bd5809d2645ab805d69ff
Julia Pak <jpak0444@gmail.com>
2022-05-23 20:05:47 -0400

    contract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.13;
 
 import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 //my contract
