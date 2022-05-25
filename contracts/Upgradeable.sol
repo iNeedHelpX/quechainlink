bacdf565ce31488fdbfcde7d5e1c833b67fffae3
Julia Pak <jpak0444@gmail.com>
2022-05-25 09:46:09 -0400

    prices

--- contracts/Upgradeable.sol
+++ contracts/Upgradeable.sol
@@ -3,16 +3,36 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-//my contract that makes other contract upgradable
-contract HeyJulia {
-    string public message;
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
 
-    constructor(string memory initialMessage) {
-        message = initialMessage;
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x9326BFA02ADD2366b30bacB125260Af641031331
+        );
     }
 
-    function updateMessage(string memory newMessage) public {
-        message = newMessage;
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int256) {
+        (
+            ,
+            /*uint80 roundID*/
+            int256 price, /*uint startedAt*/
+            ,
+            ,
+
+        ) = /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            priceFeed.latestRoundData();
+        return price;
     }
 }
