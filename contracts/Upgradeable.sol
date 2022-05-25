784ecadf5fdc13ecb7337134941c9fa1329b613b
Julia Pak <jpak0444@gmail.com>
2022-05-25 00:59:01 -0400

    kk

--- contracts/Upgradeable.sol
+++ contracts/Upgradeable.sol
@@ -4,4 +4,4 @@ pragma solidity >=0.4.22 <0.9.0;
 import "/Users/juliapak/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "/Users/juliapak/node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
-//my contract that makes upgradable
+//my contract that makes other contract upgradable
