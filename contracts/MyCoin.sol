985e58bd315427b959ea39d0058ddec5023065aa
Julia Pak <jpak0444@gmail.com>
2022-05-26 19:55:13 -0400

    added my coin

---
+++ contracts/MyCoin.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+//trialing this
+contract MyCoin {
+    //my personal coin
+
+    int256 balance;
+
+    constructor() public {
+        balance = 0;
+    }
+
+    function getBalance() public view returns (int256) {
+        return balance;
+    }
+
+    function depositBalance(int256 amt) public {
+        balance = balance + amt;
+    }
+
+    function withdrawBalance(int256 amt) public {
+        balance = balance - amt;
+    }
+}
